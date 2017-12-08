2394e29ea66522a947fc8ee00833a154cc66679f
Pontus Persson <pontus@warpinmedia.com>
2017-12-08 19:02:45 +0100

    WIP

--- client/peer-exchange.js
+++ client/peer-exchange.js
@@ -1,6 +1,7 @@
 import { createClient } from './websocket.js';
+import { dataChannelConfig, peerConnectionConfig } from './config.js';
 
-export function createPeerExchange(client, peerConnectionConfig) {
+export function createPeerExchange(client) {
 
     const listeners = new Set();
     const connections = new Set();
@@ -11,6 +12,7 @@ export function createPeerExchange(client, peerConnectionConfig) {
 
     client.listen(async signal => {
         if (signal.sdp) {
+            console.log('Signal', signal);
             if(signal.sdp.type == 'offer') {
                 const conn = await createConnection(signal.sdp);
                 listeners.forEach(callback => callback(conn));
@@ -36,6 +38,7 @@ export function createPeerExchange(client, peerConnectionConfig) {
         client.send({sdp: localDesc});
 
         conn.addEventListener('icecandidate', event => {
+            console.log('iceCandidate', event);
             if(event.candidate != null) {
                 client.send({ice: event.candidate});
             }
--- client/webrtc.js
+++ client/webrtc.js
@@ -1,4 +1,3 @@
-import { dataChannelConfig, peerConnectionConfig } from './config.js';
 import { createClient } from './websocket.js';
 import { createPeerExchange } from './peer-exchange.js';
 
@@ -6,12 +5,32 @@ var peerConnection;
 var serverConnection;
 
 async function pageReady() {
-    const peerExchange = createPeerExchange(await createClient('wss://' + window.location.hostname + ':8443'));
+    const localVideo = document.querySelector("#localVideo");
+
+    const constraints = {
+        video: true,
+        audio: true,
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    localVideo.srcObject = stream;
+
+    const server = await createClient('wss://' + window.location.hostname + ':8443');
+    const peerExchange = createPeerExchange(server);
     peerExchange.onConnection(conn => {
-        console.log(conn);
+        console.log("New Connection", conn);
+
+        conn.addEventListener("addstream", event => {
+            console.log('Stream', event);
+            const videoElement = document.createElement('video');
+            videoElement.srcObject = event.stream;
+            document.querySeletor('.streams').appendChild(videoElement);
+        });
+
+        conn.addStream(stream);
     });
 
-    console.log(peerExchange);
+    //console.log(peerExchange);
     peerExchange.extendOffer();
 }
 
